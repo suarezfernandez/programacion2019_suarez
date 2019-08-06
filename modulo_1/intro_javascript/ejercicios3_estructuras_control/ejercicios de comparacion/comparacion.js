457a0cbbc4cc5a304813ec0ce9f063efd8cf039d
luisfenandez <luisfernandez0793@gmail.com>
2019-08-06 09:54:39 -0400

    java

---
+++ modulo_1/intro_javascript/ejercicios3_estructuras_control/ejercicios de comparacion/comparacion.js
@@ -0,0 +1,31 @@
+if (10 < 40){
+    console.log("si es verdad");
+}
+
+//Operadores Aritmeticos
+console.log(2 + 3 - 1);   // 4
+console.log(4 * 3 / 2);   // 6
+console.log(11 % 3 ** 2 );  // 2
+console.log(4 / 3 * 2); // 2.666
+
+/* Ejercicio 1
+Digitar 2 numeros y mostrar su
+suma, resta, multiplicaciom, division, potencia y residuo.
+*/
+
+var numero1 = parseInt(prompt("Digite el numero1");
+var numero2 = perseInt(prompt("Digite el numero2");
+
+var suma = parseInt(numero1 + numero2;
+var resta = numero1 - parseInt(numero2;
+var multiplicaciom = numero1 * numero2;
+var division = numero1 / numero2;
+
+console.log(suma),
+console.log(resta),
+console.log(multiplicacion)),
+console.log(suma),
+console.log(suma),
+console.log(suma),
+
+
