33601efe0bcdbd8102662d7a2cb5b81d08f65e4f
luisfenandez <luisfernandez0793@gmai.com>
2019-08-06 12:23:08 -0400

    operaciones

--- modulo_1/intro_javascript/ejercicios3_estructuras_control/ejercicios de comparacion/comparacion.js
+++ modulo_1/intro_javascript/ejercicios3_estructuras_control/ejercicios de comparacion/comparacion.js
@@ -1,3 +1,4 @@
+/*
 if (10 < 40){
     console.log("si es verdad");
 }
@@ -7,25 +8,64 @@ console.log(2 + 3 - 1);   // 4
 console.log(4 * 3 / 2);   // 6
 console.log(11 % 3 ** 2 );  // 2
 console.log(4 / 3 * 2); // 2.666
+*/
 
 /* Ejercicio 1
 Digitar 2 numeros y mostrar su
 suma, resta, multiplicaciom, division, potencia y residuo.
+
+
+var numero1 = parseInt(prompt("Digite el numero1"));
+var numero2 = parseInt(prompt("Digite el numero2"));
+
+
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multiplicacion = numero1 * numero2;
+var division = numero1 / numero2;
+var potencia = numero1 ** numero2;
+var residuo = numero1 % numero2;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+console.log(potencia);
+console.log(residuo);
 */
 
-var numero1 = parseInt(prompt("Digite el numero1");
-var numero2 = perseInt(prompt("Digite el numero2");
 
-var suma = parseInt(numero1 + numero2;
-var resta = numero1 - parseInt(numero2;
-var multiplicaciom = numero1 * numero2;
-var division = numero1 / numero2;
+/*ejercicios
 
-console.log(suma),
-console.log(resta),
-console.log(multiplicacion)),
-console.log(suma),
-console.log(suma),
-console.log(suma),
+
+if (numero1 == numero2) {
+    console.log("Son iguales");
+}
+ if(numero1 != numero2) {
+    console.log("Son diferentes");
+}
+ if(numero1 > numero2) {
+    console.log("numero1 es mayor que el numero2");
+}
+ if(numero1 < numero2) {
+    console.log("numero2 es mayor que es numero1");
+}
+if (numero1 > numero2) {
+    console.log("numero1 es mayor o igual que es numero2");
+}
+
+*/
+
+var numero1 = parseInt(prompt("Digite el numero1"));
+var numero2 = parseInt(prompt("Digite el numero2"));
+
+if (numero1 > numero2) {
+    console.log(`${numero1} es el mayor`);
+}
+else {
+    console.log(`${numero2} es el mayor`);
+}
+
+
 
 
