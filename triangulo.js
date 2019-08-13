18949768bff6477c132e6d9abac0f028498571ac
suarezfernandez <luisfernandez0793@gmail.com>
2019-08-12 13:09:03 -1100

    triangule

--- triangulo.js
+++ triangulo.js
@@ -1,9 +1,8 @@
-// triangulo
+/*triangulo
 
     
 function obtenerArea() {
-    var areaResultado = document.getElementById("areaResultado");
-    var textResultado = document.getElementById("textarea1");
+   
     var b = parseInt(document.getElementById("base").value);
     var h = parseInt(document.getElementById("altura").value);
   
@@ -12,3 +11,41 @@ function obtenerArea() {
     areaResultado.innerHTML = "" + area;
     textResultado.value = area;
   }
+*/
+
+// dia de la semana
+
+var dia = parseInt(prompt(`Digite un numero para saber el dia de la Semana`));
+
+alert(`los dias son:
+1.lunes
+2.martes
+3.miercoles
+4.jueves
+5.viernes
+6.sabado
+7.domingo`);
+
+
+
+if (lunes == 1 ){
+    console.log("Lunes");
+}
+else if (martes == 2) {
+    console.log("Martes");
+}
+else if (miercoles === 3) {
+    console.log("Miercoles");
+}
+else if (jueves === 4) {
+    console.log("Jueves");
+}
+else if (viernes === 5) {
+    console.log("Viernes");
+}
+else if (sabado === 6) {
+    console.log("Sabado");
+}
+else if (domingo === 7) {
+    console.log("Domingo");
+}
