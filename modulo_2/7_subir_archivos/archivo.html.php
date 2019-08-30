e8d13b09fb5a1abb5397f6a9ccd99f236d335e71
luisfernandez <luisfernandez0793@gmail.com>
2019-08-30 11:41:17 -0400

    tamanos

--- modulo_2/7_subir_archivos/archivo.html.php
+++ modulo_2/7_subir_archivos/archivo.html.php
@@ -5,24 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Subir Archivos</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="animate.css">
 </head>
 <body>
-<h2>Subir Archivos</h2>
-<form method="post" enctype="multipart/form-data">
-<input type="file" name="archivo">
-<button type="submit" name="subir"> Subir archivo</button>
-</form>
-<?php
+<div class="container">
+        <div class="border w-50 mx-auto my-5">
+            <h1 class="display-4 text-center">Subir archivo</h1>
+            <div class="mb-4">
+            <form action="" method="post" enctype="multipart/form-data" class="form-gruop">
+                <input type="file" name="archivo" id="archivo" class="d-block my-3 mx-auto">
+            <?php 
+                if (isset($mensaje) && !empty($mensaje)) {
+                    echo "<div class='container'><p class='alert alert-success mx-auto w-75'>{$mensaje}</p></div>";
+                }
+                if (isset($error) && !empty($error)) {
+                    echo "<div class='container'><p class='alert alert-danger mx-auto w-75'>{$error}</p></div>";
+                }
+            ?>
+                <button type="submit" name="subir" class="d-block my-2 mx-auto btn btn-outline-secondary w-75 ">Subir archivo</button>
+            </form>
+            </div>
+        </div>
+        
+    </div>
 
-if (isset($mensaje)) {
-    echo "<div class='alert alert-success'> $mensaje</div>";
-}
 
-if (isset($error)) {
-    echo "<div class='alert alert-danger'>$error</div>";
-}
-?>
-    
+
 </body>
 </html>
--- modulo_2/7_subir_archivos/subir.php
+++ modulo_2/7_subir_archivos/subir.php
@@ -1,36 +1,48 @@
-<?php
+<?php 
 
 $error = "";
 $mensaje = "";
 
-// if (isset($_POST['subir'])) {
-//     echo "<pre>";
-//     print_r($_FILES);
-//     echo "</pre>";
+if(isset($_POST['subir'])) {
+    // echo "<pre>";
+    // print_r($_FILES);
+    // echo "</pre>";
 
-    if (isset($_FILES['archivo']) &&  ($_FILES['archivo']['error']) == 0 ) {
-     $nombre = $_FILES['archivo']['name'];
-     $nombre_tmp = $_FILES['archivo']['tmp_name'];
-     $tamano = $_FILES['archivo']['size'];
+    if (isset($_FILES['archivo']) && ($_FILES['archivo']['error']) == 0) {
+        $nombre = $_FILES['archivo']['name'];
+        $nombre_tmp = $_FILES['archivo']['tmp_name'];
+        $tamano = $_FILES['archivo']['size'];
 
-     if (is_uploaded_file($nombre_tmp)) {
-         $mensaje "Hemo recibido el archivo";
-     } else {
-        $mensaje "No se pudo recibir el archivo";
-     }
-// Mover el archivo a nuestra carpeta
-$movido = move_uploaded_file($nombre_tmp, "archivos_recibidos/".$nombre);
-if ($movido) {
-    $mensaje "El archivo se subio correctamente";
-} else {
-    $error "No se pudo subir correctamente";
+        if(is_uploaded_file($nombre_tmp)) {
+            $mensaje = "<div class='alert alert-success'>Hemos recibido el archivo</div>";
+
+            $directorio = 'archivos_recibidos';
+
+            if(file_exists($directorio)) {
+                echo "<div class='alert alert-primary'>El directorio existe</div>";
+
+                // Mover el archivo a nuestra carpeta, tambien sirve para verificar que no suba el archivo si el directorio no existe
+                $movido = move_uploaded_file($nombre_tmp, "archivos_recibidos/" .$nombre);
+
+                if($movido) {
+                    $mensaje = "<div class='alert alert-success'>El archivo se subio correctamente</div>";
+                }else {
+                    $error = "<div class='alert alert-success'>No se pudo mover correctamente</div>";
+                }
+                }else {
+                    echo "<div class='alert alert-warning'>El directorio no existe</div>";
+                }
+
+            
+
+        }else {
+            $error = "<div class='alert alert-danger'>No se a enviado un archivo</div>";
+        }
+        
+    }else {
+        $error = "<div class='alert alert-warning'>No se pudo recibir el archivo</div>";
+    }
 }
-    }
-    else {
-        $error "no se ha enviado un archivo";
-    }
 
-
-// Inclir la vista
-
+// Incluir la vista
 require_once 'archivo.html.php';
