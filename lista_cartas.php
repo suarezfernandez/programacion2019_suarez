b599fcb1e7032038c1590e98b9b42ea633423f8f
luisfernandez <luisfernandez0793@gmail.com>
2019-09-11 12:28:39 -0400

    ahahh

---
+++ lista_cartas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista de cartas</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <h3 class="col-md-12">
+                Lista de cartas
+                <a href="registro_cartas.php">Registrar cartas</a>
+                <hr>
+            </h3>
+
+            <div class="card col-md-3">
+                <img src="" alt="" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Nombre</h5>
+                    <p class="card-text text-success">$ 1,000</p>
+                    <a href="#" class="btn btn-primary">Ver carta</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
