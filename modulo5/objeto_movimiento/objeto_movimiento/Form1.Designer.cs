d15ac4f2f4cdc52e04580a0378381a74aae8c0d6
suarezfernandez <luisfernandez0793@gmail.com>
2019-10-11 12:03:23 -0400

    VIERNES

--- modulo5/objeto_movimiento/objeto_movimiento/Form1.Designer.cs
+++ modulo5/objeto_movimiento/objeto_movimiento/Form1.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Aqua;
+            this.label1.ForeColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(22, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 400);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "fondo";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Fuchsia;
+            this.label2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label2.Location = new System.Drawing.Point(193, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "cuadrado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- modulo5/objeto_movimiento/objeto_movimiento/Form1.cs
+++ modulo5/objeto_movimiento/objeto_movimiento/Form1.cs
@@ -16,5 +16,69 @@ namespace objeto_movimiento
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            var keyValue = e.KeyValue;
+
+          //  MessageBox.Show(keyValue.ToString());
+            var posicionX = label2.Location.X;
+            var posicionY = label2.Location.Y;
+
+           // MessageBox.Show(posicionX.ToString());
+            //MessageBox.Show(posicionY.ToString());
+
+       
+               
+
+            if (keyValue == 37)
+             {
+              //  MessageBox.Show("Arriba");
+                label2.Location = new System.Drawing.Point(posicionX - 1, posicionY);
+
+             }
+                
+        // Izquierda
+            if (keyValue == 38)
+            {
+               // MessageBox.Show("Izquierda");
+            }
+            label2.Location = new System.Drawing.Point(posicionY - 1, posicionX);
+            
+            if (keyValue == 39)
+            {
+                 //MessageBox.Show("Derecha");
+            
+            label2.Location = new System.Drawing.Point(posicionX + 1, posicionY);
+            }
+               
+            if (keyValue == 40)
+            {
+                //MessageBox.Show("Abajo");
+            }
+                
+
+        
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
     }
 }
